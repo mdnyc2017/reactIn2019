729cace709d8a4c06eb07c39672162ab1d353c8a
Michael Douglas <michaelseandouglas@gmail.com>
2019-12-01 16:17:51 -0500

    updating app collectionItem and collectionPreview components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shopPage/shopPage.component'
 import './App.css';
 
-function App() {
-  return (
-    <div >
-     <HomePage />
-    </div>
-  );
+
+function App(){
+    return (
+      <div className='App'>
+        <Switch>
+          <Route exact path='/'component={HomePage} />
+          <Route exact path='/shop'component={ShopPage} />
+        </Switch>
+      </div>
+    );
 }
 
 export default App;
