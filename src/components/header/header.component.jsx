dc70c305baa04bb1259cf42b3fd7704700b3ed81
Michael Douglas <michaelseandouglas@gmail.com>
2020-02-17 15:24:02 -0500

    Continuing Redux integration, adding cartIcon and  cartDropdown components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className='header'>
 		<Link to='/' className='logo-container'>
 			<Logo className='logo' />
@@ -17,14 +20,23 @@ const Header = ({ currentUser }) => (
 				CONTACT
 			</Link>
 			{currentUser ? (
-				<div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+				<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
 			) : (
-				<Link to='/signIN' className='option'>
-                    SIGN IN
+					<Link to='/signIN' className='option'>
+						SIGN IN
 				</Link>
-			)}
+				)}
+				<CartIcon/>
 		</div>
+		{
+			hidden ? null 
+			: <CartDropdown />
+		}
 	</div>
 );
 
-export default Header;
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+	currentUser,
+	hidden,
+})
+export default connect(mapStateToProps)(Header);
