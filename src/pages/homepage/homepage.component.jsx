805b163e2058439a3f6147e466c284828ba13acd
Michael Douglas <michaelseandouglas@gmail.com>
2020-03-08 16:40:59 -0400

    updating components styling

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import { HomePageContainer } from './homepage.styles'
 import Directory from '../../components/directory/directory.component';
 // import MenuItem from '../../components/menu-item/menu-item.component';
 
-import './homepage.styles.scss';
 
 const HomePage = () => (
-    <div className='homepage'>
+    <HomePageContainer>
         <Directory />
-    </div>
+    </HomePageContainer>
 );
 
 export default HomePage;
